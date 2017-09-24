1d2bd97c6a3a00f1c81d6a4d3c072527cf7bd85d
Angel Coco Chen <angel@finaxar.com>
2017-09-24 22:04:31 +0800

    Basic route and component scaffold.

---
+++ src/components/Resources.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+export default () => {
+  return (
+    <div>
+      Super Secret Stuff
+      <ul>
+        <li>Secret</li>
+        <li>Stuff</li>
+        <li>Yo</li>
+      </ul>
+    </div>
+  )
+}
